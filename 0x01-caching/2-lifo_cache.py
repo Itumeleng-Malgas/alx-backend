10bf201d792008435c7114989ab0fce90aaa7b7a
Itumeleng Malgas <aaron.tumi@live.co.za>
2024-05-09 02:40:14 +0200

    2-lifo_cache.py

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -12,15 +12,17 @@ class LIFOCache(BaseCaching):
     def __init__(self):
         """ Initialize LIFOCache """
         super().__init__()
+        self.stack = []
 
     def put(self, key, item):
         """ Add an item in the cache """
         if key is not None and item is not None:
             if len(self.cache_data) >= self.MAX_ITEMS:
-                discarded_key = next(reversed(self.cache_data))
+                discarded_key = self.stack.pop()
                 del self.cache_data[discarded_key]
                 print("DISCARD:", discarded_key)
             self.cache_data[key] = item
+            self.stack.append(key)
 
     def get(self, key):
         """ Get an item by key """
